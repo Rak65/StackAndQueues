d1e337c17e419e113f179f2d1ddf4fc326dbe2e7
Rak65 <rakeshp6505@gmail.com>
2023-05-14 22:22:50 +0530

    Ability to create a Stack.

---
+++ StackAndQueues/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackAndQueues
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Create a Stack.");
+            LinkedList stack = new LinkedList();
+            stack.Push(56);
+            stack.Push(30);
+            stack.Push(70);
+            stack.Display();
+            Console .ReadKey();
+        }
+    }
+}
